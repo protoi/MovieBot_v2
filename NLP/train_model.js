728a625c5c52a6a1bb48b3a0ca683eb33fa9e14a
protoi <protikdatta9@gmail.com>
2023-02-08 10:15:45 +0530

    corpus seems to be working now, added some test cases to train_model.js but we can convert this to a mocha/chai unit test

--- NLP/train_model.js
+++ NLP/train_model.js
@@ -15,26 +15,65 @@ const { dockStart } = require("@nlpjs/basic");
 
   const manager = dock.get("nlp");
 
- /*  const queries = [
-    "Who was the actor in Rise of the planet of the apes",
-    "What genre is RIse of the planet of the apes",
-    "is rise of the planet of the apes a comedy movie",
-    "who were the actors in Spiderman",
-    "name of the girl in Avengers age of ultron",
-    "what was the genre of Titanic",
+  const queries = [
+    "Who was the actor in 'Rise of the planet of the apes'",
+    "What genre is 'RIse of the planet of the apes'",
+    "is 'rise of the planet of the apes' a comedy movie",
+    "who were the actors in 'Spiderman'",
+    "name of the girl in 'Avengers age of ultron'",
+    "what was the genre of 'Titanic'",
     "show me action and horror movies of tom hanks and johnny depp",
-    "when did titanic come out",
-    "which year did rise of the planet of the apes get released",
-    "which year did avengers age of ultron get released",
+    "when did 'titanic' come out",
+    "which year did 'rise of the planet of the apes' get released",
+    "which year did 'avengers age of ultron' get released",
     "release date of action and horror movie starring tom hanks",
-  ]; */
-  const answers = {};
+    "show me actor list of 'rise of the planet of the apes'",
+    "list of actors in 'superman returns'",
+    "In which year 'SpiderMan 2' was released",
+  ];
+  const answers = [
+    "message.get_actor",
+    "message.get_genre",
+    "message.get_genre",
+    "message.get_actor",
+    "message.get_actor",
+    "message.get_genre",
+    "message.get_movie",
+    "message.get_movie_year",
+    "message.get_movie_year",
+    "message.get_movie_year",
+    "message.get_movie_year",
+    "message.get_actor",
+    "message.get_actor",
+    "message.get_movie_year",
+  ];
   // Train the network
   await manager.train();
   manager
     .save("mymodel.nlp")
-    .then((response) => {
+    .then(async (response) => {
       console.log("model written");
+      for (let index = 0; index < queries.length; index++) {
+        const Q = queries[index];
+        const answer = answers[index];
+        try {
+          const results = await manager.process("en", Q);
+          console.log(
+            `==========${Q}==========\n${
+              results.classifications[0].intent
+            }\n======================\n${
+              results.classifications[0].intent === answer
+              //   JSON.stringify(
+              //   // results.entities,
+              //   null,
+              //   2
+              // )
+            }`
+          );
+        } catch (err) {
+          console.log(`${Q} FAILED`);
+        }
+      }
       //   queries.forEach((Q) => {
       //     manager
       //       .process("en", Q)

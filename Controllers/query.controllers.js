c93d517c1b001faa1a368ee14334eb555c5dc808
Ghutoon <niladrichatterjeen@gmail.com>
2023-02-10 11:43:10 +0530

    Modifying a few things

--- Controllers/query.controllers.js
+++ Controllers/query.controllers.js
@@ -4,28 +4,21 @@ const { logger } = require("../logger");
 const { aggregate } = require("../model");
 const Query = require("../model");
 
-
-
-
-
-
 //This function takes an intent as parameter and gives those documents from mongo DB where the intent matches
 
-
 const get_document_on_the_basis_of_intents = async (request, response) => {
-
+  let query = null;
   let intent = request.query.intent;
   try {
-    const query = await Query.find({
+    query = await Query.find({
       /* "EntityIntent_tuple": {
         "intents": "message.get_actor",
       }, */
 
       "EntityIntent_tuple.intents": intent,
     });
-  }
-  catch (err) {
-    logger.error("Could not fetch data")
+  } catch (err) {
+    logger.error("Could not fetch data");
   }
   //console.log(query);
   try {
@@ -35,14 +28,12 @@ const get_document_on_the_basis_of_intents = async (request, response) => {
   }
 };
 
-
-
 //This Function groups the documents on the basis of intents and gives their respective count
 
 const group_documents_by_intent = async (request, response) => {
-
+  let query = null;
   try {
-    const query = await Query.aggregate([
+    query = await Query.aggregate([
       {
         $group: {
           _id: "$EntityIntent_tuple.intents",
@@ -51,12 +42,11 @@ const group_documents_by_intent = async (request, response) => {
           },
           count: {
             $sum: 1,
-          }
+          },
         },
       },
     ]);
-  }
-  catch (err) {
+  } catch (err) {
     logger.error("Could not fetch data");
   }
 
@@ -67,23 +57,33 @@ const group_documents_by_intent = async (request, response) => {
   }
 };
 
-
-
-
-
-
 //This function takes a date in yyyy-mm-dd format
 //Then it gets the starting date and ending date of the week in which the input date lies
 //Then it groups document on the basis of the dates of the week
 
-
-
 const group_queries_by_date_week = async (request, response) => {
-  let date = request.query.date;
-  let start_date = "2023-02-01";
-  let end_date = "2023-02-09";
+  let start_date = null;
+  let end_date = null;
   try {
-    const query = await Query.aggregate([
+    let date = new Date(request.query.date);
+    let first = date.getDate() - date.getDay() + 1;
+    let last = first + 6;
+    let firstday = new Date(date.setDate(first)).toISOString();
+    let lastday = new Date(date.setDate(last)).toISOString();
+    start_date = firstday.substring(0, 10);
+    end_date = lastday.substring(0, 10);
+  } catch (err) {
+    response.send(err.message);
+    logger.error("Date format error");
+    return;
+  }
+
+  /* console.log(start_date);
+    console.log(end_date); */
+
+  let query = null;
+  try {
+    query = await Query.aggregate([
       {
         $group: {
           _id: {
@@ -127,36 +127,34 @@ const group_queries_by_date_week = async (request, response) => {
         },
       },
     ]);
-  }
-  catch (err) {
-    logger.error("Could not fetch data")
+  } catch (err) {
+    logger.error("Could not fetch data");
   }
   try {
     response.send(query);
   } catch (error) {
     response.status(500).send(error);
   }
-}
+};
 
 ///This function is not useful for now, ignore it
 
 const get_documents_within_given_time_frame = async (request, response) => {
-    let t = request.query.time; // -> 2023-02-08TO03:35:34+00:00
-    
-    console.log(t);
-    const regex = /(\d{4}-\d{2}-\d{2}T)(\d{2}):\d{2}:\d{2}\.\d{3} (.*)/;
+  let t = request.query.time; // -> 2023-02-08TO03:35:34+00:00
 
-    const x = t.match(regex);
+  console.log(t);
+  const regex = /(\d{4}-\d{2}-\d{2}T)(\d{2}):\d{2}:\d{2}\.\d{3} (.*)/;
 
-    // console.log(`${x[1]}${x[2]}:00:00.000+${x[3]}`);
+  const x = t.match(regex);
 
+  // console.log(`${x[1]}${x[2]}:00:00.000+${x[3]}`);
 
   //   let ending_time = request.query.et;
 
-    let start = `2023-02-05T00:00:00.000+00:00`;
-    let end = `2023-02-09T00:00:00.+00:00`;
-    
-//   let end = "2023-02-08T05:59:59.999+00:00";
+  let start = `2023-02-05T00:00:00.000+00:00`;
+  let end = `2023-02-09T00:00:00.+00:00`;
+
+  //   let end = "2023-02-08T05:59:59.999+00:00";
 
   // let end = "2023-02-08T" + "03" + ":" + "59" + ":" + "59.999" + "+00:00";
 
@@ -168,56 +166,53 @@ const get_documents_within_given_time_frame = async (request, response) => {
     },
   });
 
-    try {
-      response.send(query)
+  try {
+    response.send(query);
     //response.send(`between ${start} and ${end} : ${query.length}`);
   } catch (error) {
     response.status(500).send(error);
   }
 };
 
-
 //This function fetches genre array from mongo DB documents
 //Then returns a map containing the names of the genres as key and its frequency and value
 
 const get_genre_frequencies = async (request, response) => {
+  let query = null;
   try {
-    const query = await Query.find(
+    query = await Query.find(
       { "EntityIntent_tuple.entities.genre": { $ne: [] } },
       {
         "EntityIntent_tuple.entities.genre": 1,
-        "_id": 0,
+        _id: 0,
       }
     );
-  }
-  catch (err) {
+  } catch (err) {
     logger.error("Could not fetch data");
+    response.send(err.message);
+    return;
   }
   let freq_map = new Map();
-    query.forEach(element => {
+  query.forEach((element) => {
     let genre = element["EntityIntent_tuple"]["entities"]["genre"];
-      genre.forEach(element => {
-        if (freq_map[element] == null)
-          freq_map[element] = 1;
+    genre.forEach((element) => {
+      if (freq_map[element] == null) freq_map[element] = 1;
       freq_map[element]++;
     });
-  }); 
+  });
 
   try {
     response.send(freq_map);
   } catch (error) {
     response.status(500).send(error);
-  } 
-}
-
-
-
+  }
+};
 
 //Exporting the required functions
 module.exports = {
   get_document_on_the_basis_of_intents,
-  group_documents_by_intent,
   get_documents_within_given_time_frame,
   group_queries_by_date_week,
   get_genre_frequencies,
+  group_documents_by_intent,
 };
